cbf071f02f07f84d803be0592891d79ecb6d80a6
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2024-09-02 09:41:09 +0200

    chore: update deps

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="ba-pns",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         'networkx==3.1',
         'numpy==1.24.1',
-        'matplotlib==3.7.2',
+        'matplotlib==3.9.2',
         'powerlaw==1.5',
         'pandas==2.2.2',
     ],
